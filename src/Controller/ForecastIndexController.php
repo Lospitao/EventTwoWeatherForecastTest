4213a64fafa3a21442561edfd94f090d5dac5576
Laura Lospitao <lauralospitao@gmail.com>
2022-01-25 12:59:18 +0100

    Final version

---
+++ src/Controller/ForecastIndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForecastIndexController extends AbstractController
+{
+    /**
+     * @Route("/forecast_index", name="forecast_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('forecast_index/index.html.twig', [
+            'controller_name' => 'ForecastIndexController',
+            'apiHost' => $this->getParameter('api_host'),
+        ]);
+    }
+}
